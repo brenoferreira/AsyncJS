606e081c6193207891ec3908a92a984512e145eb
Breno Ferreira <bferreira@windowslive.com>
2013-05-18 21:09:03 -0300

    Main views

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/jquery', routes.jquery);
+app.get('/rx', routes.rx);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,11 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.jquery = function(req, res){
+    res.render('jquery', { title: '$.Deferred' });
+};
+
+exports.rx = function(req, res){
+    res.render('rx', { title: 'Rx.JS' });
+};
